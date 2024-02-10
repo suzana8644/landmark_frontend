1916015d358e017f050243d270a747f979132c01
suzana8644 <phuyalsujana17@gmail.com>
2024-02-10 21:39:08 +0545

    add: admin dashboard updated with nav links

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,16 +1,22 @@
 "use client";
 
 import React, { useRef } from "react";
-import { useRouter} from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 const Dashboard = () => {
     const router = useRouter();
 
     console.log(router);
     const playerRef = useRef(null);
+    const user = window.localStorage.getItem("token");
+
+    if (!user) {
+        router.push("/admin/login");
+    }
+
     return (
         <div className="flex flex-col gap-10">
-            <span className="text-3xl font-black text-center">Admin Dashboard</span>
+            Dashboard page
         </div>
     );
 };
