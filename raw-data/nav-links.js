d7d0a6e24ea3aadfb82ac12356ad021f3d7cc66b
Dhiraj Phuyal <phuyal.dhiraj17@gmail.com>
2024-02-19 16:03:14 +0545

    refractor: change admin nav links

--- raw-data/nav-links.js
+++ raw-data/nav-links.js
@@ -1,41 +1,41 @@
 export const navLinks = [
-    {
-        path: "/about-us",
-        name: "About Us",
-    },
-    {
-        path: "/eventCategories",
-        name: "Events",
-    },
-    {
-        path: "/gallery",
-        name: "Gallery",
-    },
-    {
-        path: "/connect",
-        name: "Connect",
-    },
+  {
+    path: "/about-us",
+    name: "About Us",
+  },
+  {
+    path: "/eventCategories",
+    name: "Events",
+  },
+  {
+    path: "/gallery",
+    name: "Gallery",
+  },
+  {
+    path: "/connect",
+    name: "Connect",
+  },
 ];
 
 export const adminNavLinks = [
-    {
-        path: "/admin/dashboard",
-        name: "Dashboard",
-    },
-    {
-        path: "/admin/dashboard/event",
-        name: "Events",
-    },
-    {
-        path: "/admin/dashboard/gallery",
-        name: "Gallery",
-    },
-    {
-        path: "/admin/dashboard/decoration",
-        name: "Decorations",
-    },
-    {
-        path: "/admin/dashboard/user",
-        name: "Users",
-    },
+  {
+    path: "/admin/dashboard",
+    name: "Dashboard",
+  },
+  {
+    path: "/admin/dashboard/event",
+    name: "Events",
+  },
+  {
+    path: "/admin/dashboard/gallery",
+    name: "Edit Event Categories",
+  },
+  {
+    path: "/admin/dashboard/decoration",
+    name: "Edit Themes",
+  },
+  {
+    path: "/admin/dashboard/organizers",
+    name: "Edit Organizers",
+  },
 ];
